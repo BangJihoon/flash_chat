48b14bbed5c7cd3885772d7f0470fe402bde8f7c
Bangjihoon <jihoon289@naver.com>
2022-04-20 22:59:28 +0900

    feat - spinner 추가 : ModalProgressHUD (nullSafety)

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,8 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static String id = 'LoginScreen';
@@ -15,64 +15,72 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
+  bool _saving = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: _saving,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 150.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: "Enter your email"),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: "Enter your password"),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              "Log in",
-              Colors.lightBlueAccent,
-              () async {
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
-                  if (user != null) {
-                    Navigator.popAndPushNamed(context, ChatScreen.id);
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: "Enter your email"),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: "Enter your password"),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                "Log in",
+                Colors.lightBlueAccent,
+                () async {
+                  setState(() {
+                    _saving = true;
+                  });
+
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      Navigator.popAndPushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-            )
-          ],
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'RegistrationScreen';
@@ -12,7 +13,7 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
-  bool showSpinner = false;
+  bool _saving = false;
   late String email;
   late String password;
 
@@ -20,66 +21,76 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 150.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: _saving,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 150.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.left,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: "Enter your Email"),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.left,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: "Enter your password"),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              "Register",
-              Colors.blueAccent,
-              () async {
-                // 비동기식 메소드이므로 async 함수로 만들어서 해당메소드가 종료될때까지 기다려야 하며, try가 필요
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  if (newUser != null) {
-                    Navigator.popAndPushNamed(context, ChatScreen.id);
-                    // TODO: 이렇게 채팅으로 진입해도 괜찮은가?.. 뒤로가게되면..;?
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.left,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: "Enter your Email"),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.left,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: "Enter your password"),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                "Register",
+                Colors.blueAccent,
+                () async {
+                  setState(() {
+                    _saving = true;
+                  });
+                  // 비동기식 메소드이므로 async 함수로 만들어서 해당메소드가 종료될때까지 기다려야 하며, try가 필요
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
+                      Navigator.popAndPushNamed(context, ChatScreen.id);
+                      // TODO: 이렇게 채팅으로 진입해도 괜찮은가?.. 뒤로가게되면..;?
+                    }
+                  } catch (e) {
+                    // TODO: print말고 사용자에게 alert하도록 변경이 필요해보임
+                    print(e);
+                  } finally {
+                    setState(() {
+                      _saving = false;
+                    });
                   }
-                } catch (e) {
-                  // TODO: print말고 사용자에게 alert하도록 변경이 필요해보임
-                  print(e);
-                }
-                ;
-              },
-            ),
-          ],
+                  ;
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
